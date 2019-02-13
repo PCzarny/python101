b2093d15a8179339f458a1913150193afaa57793
Piotr Czarny <piotr.a.czarny@gmail.com>
2019-02-13 23:38:22 +0100

    [Lesson 5][Flask][Quiz] Add form POST handling with flash message

--- 5_flask/quiz/app.py
+++ 5_flask/quiz/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -13,8 +14,17 @@ DATA = [{
     'correct': 'Madrit'
 }]
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index() :
+    if request.method == 'POST':
+        score = 0
+        answers = request.form
+
+        for i, answer in answers.items():
+            if answer == DATA[int(i)]['correct']:
+                score += 1
+        flash('Your score: {0}'.format(score))
+        return redirect(url_for('index'))
     return render_template('index.html', questions=DATA)
 
 @app.route('/hello')
