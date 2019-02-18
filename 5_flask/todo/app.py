267eab942e2fecc14e8ff80be5553d0472a99b56
Piotr Czarny <piotr.a.czarny@gmail.com>
2019-02-18 02:52:40 +0100

    [Lesson 5][Flask][ToDo] Add styles and handle POST requests

--- 5_flask/todo/app.py
+++ 5_flask/todo/app.py
@@ -3,6 +3,9 @@ from flask import render_template
 import os
 import sqlite3
 
+from datetime import datetime
+from flask import flash, redirect, url_for, request
+
 app = Flask(__name__)
 
 app.config.update(dict(
@@ -28,12 +31,34 @@ def close_db(error):
 def index():
     return render_template('index.html')
 
-@app.route('/tasks')
+@app.route('/tasks', methods=['GET', 'POST'])
 def tasks():
+    error = None
+    if request.method == 'POST':
+        description = request.form['task'].strip()
+        if len(description) > 0:
+            db = get_db()
+            db.execute('INSERT INTO tasks VALUES (?, ?, ?, ?);',
+                        [None, description, '0', datetime.now()])
+            db.commit()
+            flash('New task added!')
+            return redirect(url_for('tasks'))
+
+        error = 'Task\'s description can\'t be empty'
+
     db = get_db()
     tasks = db.execute('SELECT * FROM tasks ORDER BY added_at DESC;').fetchall()
-    return render_template('task_list.html', tasks=tasks)
+    return render_template('task_list.html', tasks=tasks, error=error)
 
+@app.route('/done', methods=['POST'])
+def mark_as_done():
+    task_id = request.form['id']
+    db = get_db()
+    db.execute('UPDATE tasks SET done=1 WHERE id=?', [task_id])
+    db.commit()
+    flash('Status changed!')
+
+    return redirect (url_for('tasks'))
 
 if (__name__ == '__main__'):
     app.run(debug=True)
